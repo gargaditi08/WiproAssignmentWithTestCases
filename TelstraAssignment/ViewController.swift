57d437603a92a00eb676e8daba73382f7ef08a11
Aditi <gargaditi08>
2020-04-15 14:48:37 +0530

    Initial Commit

---
+++ TelstraAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TelstraAssignment
+//
+//  Created by Aditi Garg on 15/04/20.
+//  Copyright © 2020 Aditi Garg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
